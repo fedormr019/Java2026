c742ed33a3c9f7aefd0762bcff4ba7db23d2d645
fedormr019 <fedormrv@gmail.com>
2026-05-31 02:37:14 +0300

    Оператор условного перехода

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+<<<<<<< Updated upstream
 # Java2026
+=======
+Запишите следующий код через конструкцию ? :
+
+int i = 0;
+if (a == 1) {
+    i = 1;
+} else if (a == 2) {
+    i = 2;
+} else {
+    i = 3; 
+}
+>>>>>>> Stashed changes
---
+++ ToSend/src/Main.java
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+        System.out.println(i);
+    }
+}
+>>>>>>> Stashed changes
